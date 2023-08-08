0bc06622bb3ddc3b99d9757fa8e2ab75d3de5771
Derrick Timmermans <derrick.timmermans@outlook.com>
2023-08-08 13:06:02 +0200

    Add update manager

---
+++ osu.Game.Rulesets.Sentakki/SentakkiUpdateManager.cs
@@ -0,0 +1,20 @@
+using osu.Game.Updater;
+using osu.Framework.Allocation;
+using System;
+
+namespace osu.Game.Rulesets.Sentakki;
+
+public partial class SentakkiUpdateManager : SimpleGameUpdateManager
+{
+    protected override string GitHubUrl => "https://api.github.com/repos/LumpBloom7/sentakki/releases/latest";
+
+    protected override string UpdatedComponentName => "sentakki";
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        var assemblyVersion = typeof(SentakkiUpdateManager).Assembly?.GetName().Version ?? new Version();
+
+        Version = $@"{assemblyVersion.Major}.{assemblyVersion.Minor}.{assemblyVersion.Build}";
+    }
+}
